f2ce0eeda1bce0a8fd7eb5ebef70bb409a4472ba
J/A <archer884@gmail.com>
2015-02-19 01:27:47 -0600

    fixed comment in shell script

--- challenge.sh
+++ challenge.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 # I am sooooo annoyed that this same script 
-# amounts to `2015..2025` | ./target/computus
+# amounts to `2015..2025 | ./target/computus`
 # in Powershell. 
 
 for i in {2015..2025}
